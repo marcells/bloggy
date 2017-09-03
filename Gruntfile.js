2182227a3d1b28f0a1225dd767262c5242d0e1d3
Marcell Spies <marcells@gmx.de>
2017-09-03 14:17:16 +0200

    Removed coveralls stuff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,30 +9,13 @@ module.exports = function(grunt) {
 							configFile: 'eslint.json'
 						}
         },
-        clean: {
-            coverage: {
-                src: ['coverage/']
-            }
-        },
-        copy: {
-            coverage: {
-                src: ['test/**'],
-                dest: 'coverage/'
-            }
-        },
-        blanket: {
-            coverage: {
-                src: ['lib/'],
-                dest: 'coverage/lib/'
-            }
-        },
         mochaTest: {
             library: {
                 options: {
                     reporter: 'spec',
                     require: [ 'should' ]
                 },
-                src: ['coverage/test/**/*.js']
+                src: ['test/**/*.js']
             },
             watch: {
                 options: {
@@ -40,37 +23,6 @@ module.exports = function(grunt) {
                     require: [ 'should' ]
                 },
                 src: ['test/**/*.js']
-            },
-            coverage: {
-                options: {
-                    reporter: 'html-cov',
-                    require: [ 'should' ],
-                    quiet: true,
-                    captureFile: 'coverage.html'
-                },
-                src: ['coverage/test/**/*.js']
-            },
-            'travis-cov': {
-                options: {
-                    reporter: 'travis-cov'
-                },
-                src: ['coverage/test/**/*.js']
-            },
-            'mocha-lcov-reporter': {
-                options: {
-                    reporter: 'mocha-lcov-reporter',
-                    quiet: true,
-                    captureFile: 'lcov.info'
-                },
-                src: ['coverage/test/**/*.js']
-            }
-        },
-        coveralls: {
-            options: {
-                force: true
-            },
-            all: {
-                src: 'lcov.info'
             }
         },
         watch : {
@@ -85,14 +37,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-blanket');
-    grunt.loadNpmTasks('grunt-coveralls');
 
-    grunt.registerTask('build', ['clean', 'blanket', 'copy']);
-    grunt.registerTask('test', ['build', 'mochaTest:library', 'mochaTest:coverage', 'mochaTest:travis-cov', 'mochaTest:mocha-lcov-reporter']);
+    grunt.registerTask('test', [ 'mochaTest:library' ]);
 	  grunt.registerTask('default', ['eslint', 'test']);
     grunt.registerTask('code', ['eslint', 'mochaTest:watch']);
-    grunt.registerTask('ci', ['default', 'coveralls']);
+    grunt.registerTask('ci', ['default' ]);
 };
